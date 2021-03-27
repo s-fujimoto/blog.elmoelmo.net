866e694ec9d5618c9fb01a456bdcf678af9fd6d5
Shinji Fujimoto <a04316@gmail.com>
2021-03-27 16:18:23 +0900

    use node 12

---
+++ cdk/lib/elmoelmo-blog-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from '@aws-cdk/core';
+import * as amplify from '@aws-cdk/aws-amplify';
+
+export class ElmoelmoBlogStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const amplifyApp = new amplify.App(this, "elmoelmo-blog-app", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "s-fujimoto",
+        repository: "blog.elmoelmo.net",
+        oauthToken: cdk.SecretValue.secretsManager("prod/blog.elmoelmo.net/amplify", {
+          jsonField: "GITHUB_OAUTH_TOKEN",
+        }),
+      }),
+    });
+    const masterBranch = amplifyApp.addBranch("master");
+    
+    // new amplify.CfnDomain(this, "elmoelmo-blog-app-domain", {
+    //   appId: amplifyApp.appId,
+    //   domainName: "elmoelmo.net",
+    //   subDomainSettings: [
+    //     {
+    //       branchName: "master",
+    //       prefix: "blog",
+    //     }
+    //   ],
+    // })
+  }
+}
